05f89af5df65fb51df58eaceddbfc9e2cc182854
seyoung5744 <seyoung574458@gmail.com>
2025-11-07 23:29:20 +0900

    refactor: 쿠폰 Status AVAILABLE -> ISSUED

--- src/main/resources/db/migration/V9__alter_coupon_histories_table.sql
+++ src/main/resources/db/migration/V9__alter_coupon_histories_table.sql
@@ -1,27 +1,37 @@
 -- 인덱스 삭제
 ALTER TABLE coupon_histories
-    DROP INDEX idx_member_id,
-    DROP INDEX idx_store_id,
-    DROP INDEX idx_coupon_event_id,
-    DROP INDEX idx_used_at;
+DROP INDEX idx_member_id,
+DROP INDEX idx_store_id,
+DROP INDEX idx_coupon_event_id,
+DROP INDEX idx_used_at;
 
 -- 불필요한 컬럼 삭제
 ALTER TABLE coupon_histories
-    DROP COLUMN total_count,
-    DROP COLUMN event_name,
-    DROP COLUMN member_name,
-    DROP COLUMN store_name,
-    DROP COLUMN store_address,
-    DROP COLUMN event_start_at,
-    DROP COLUMN event_end_at,
-    DROP COLUMN issued_at,
-    DROP COLUMN used_at,
-    DROP COLUMN expire_at;
+DROP
+COLUMN total_count,
+    DROP
+COLUMN event_name,
+    DROP
+COLUMN member_name,
+    DROP
+COLUMN store_name,
+    DROP
+COLUMN store_address,
+    DROP
+COLUMN event_start_at,
+    DROP
+COLUMN event_end_at,
+    DROP
+COLUMN issued_at,
+    DROP
+COLUMN used_at,
+    DROP
+COLUMN expire_at;
 
 -- 컬럼 추가
 ALTER TABLE coupon_histories
-    ADD COLUMN coupon_id     BIGINT                              NOT NULL COMMENT '쿠폰 ID',
-    ADD COLUMN coupon_status ENUM ('ISSUED', 'USED', 'CANCELED') NOT NULL COMMENT '쿠폰 상태';
+    ADD COLUMN coupon_id BIGINT NOT NULL COMMENT '쿠폰 ID',
+    ADD COLUMN coupon_status ENUM ('ISSUED', 'USED', 'EXPIRED','CANCELED') NOT NULL COMMENT '쿠폰 상태';
 
 -- 테이블 설명 변경
 ALTER TABLE coupon_histories
